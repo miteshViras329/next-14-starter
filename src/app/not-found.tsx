86ac1db857ba5b1346c0469537e49a0bcf670aa1
Mitesh Appstonelab <mitesh.viras@dev.appstonelab.com>
2024-07-31 10:30:04 +0530

    changes in common layout and done some changes in error page.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const NotFound = () => {
   return (
     <div className="bg-gray-200 w-full px-16 md:px-0 h-screen flex items-center justify-center">
@@ -11,8 +13,9 @@ const NotFound = () => {
         <p className="text-gray-500 mt-4 pb-4 border-b-2 text-center">
           Sorry, the page you are looking for could not be found.
         </p>
-        <a
-          href="#"
+
+        <Link
+          href="/"
           className="flex items-center space-x-2 bg-blue-600 hover:bg-blue-700 text-gray-100 px-4 py-2 mt-6 rounded transition duration-150"
           title="Return Home"
         >
@@ -29,7 +32,7 @@ const NotFound = () => {
             ></path>
           </svg>
           <span>Return Home</span>
-        </a>
+        </Link>
       </div>
     </div>
   );
